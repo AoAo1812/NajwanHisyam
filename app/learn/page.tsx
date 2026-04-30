fb09c149b380a518e85b9a6b98de54582060770c
AoAo1812 <najwanyusuf8@gmail.com>
2026-04-30 12:23:01 +0800

    lanjutan

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,14 @@
-export default function learnpage{} (
-    return <p> ini adalah halaman learn </p>
-)
+export default function learnpage() {
+    return (
+        <div className=" bg-purple-300"> 
+            <div className=" bg-orange-300"> 
+                <img/>
+                <p> Design </p>
+                </div>
+                <div>
+                    <h1> FIGMA </h1>
+                    <p> 8 hours' ago </p>
+            </div>
+        </div>
+    )
+}
