6327bfbafcff1acfd2113ac1d72fa51fcb6c24aa
AoAo1812 <najwanyusuf8@gmail.com>
2026-04-30 15:14:51 +0800

    lanjutan

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,14 +1,39 @@
 export default function learnpage() {
     return (
-        <div className=" bg-purple-300"> 
-            <div className=" bg-orange-300"> 
-                <img/>
+    <div className=" bg-white h-screen flex gap-4 justify-center" >
+        <div className=" bg-purple-300 h-72 w-100 flex flex-col justify-between p-8 rounded-4xl text-black"> 
+            <div className=" flex gap-4"> 
+                <img src="/palette.svg" />
                 <p> Design </p>
                 </div>
-                <div>
-                    <h1> FIGMA </h1>
-                    <p> 8 hours' ago </p>
+                <div className="">
+                    <h1 className="text-4x1 font-bold ">FIGMA</h1>
+                    <p className="text-lg"> 8 hours' ago </p>
             </div>
         </div>
+
+        <div className=" bg-blue-300 h-72 w-100 flex flex-col justify-between p-8 rounded-4xl text-black"> 
+            <div className=" flex gap-4"> 
+                <img src="/palette.svg" />
+                <p> Design </p>
+                </div>
+                <div className=" ">
+                    <h1 className="text-4x1 font-bold ">PYTHON</h1>
+                    <p className="text-lg"> 8 hours' ago </p>
+            </div>
+        </div>
+
+        <div className=" bg-yellow-300 h-72 w-100 flex flex-col justify-between p-8 rounded-4xl text-black"> 
+            <div className="flex gap-4"> 
+                <img src="/palette.svg" />
+                <p> Design </p>
+                </div>
+                <div className=" ">
+                    <h1 className="text-4x1 font-bold ">CSS</h1>
+                    <p className="text-lg"> 8 hours' ago </p>
+            </div>
+        </div>
+    </div>
+    
     )
 }
